794a9c0e0a75e72410f0fe8f8466726753aaeda3
ainhoa <ainhoa.c.garay@gmail.com>
2025-10-07 12:30:02 -0500

    Develop branch creation

--- app/src/main/java/com/example/adminlivria/presentation/MainActivity.kt
+++ app/src/main/java/com/example/adminlivria/presentation/MainActivity.kt
@@ -11,7 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.adminlivria.ui.theme.AdminLivriaTheme
+import com.example.adminlivria.presentation.ui.theme.AdminLivriaTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
