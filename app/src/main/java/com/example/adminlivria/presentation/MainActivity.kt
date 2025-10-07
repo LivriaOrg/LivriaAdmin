c2152b3f0429e89b0392f18a0a5711bf7ff8ed71
kistoo <tomio.nakamura15@gmail.com>
2025-10-07 10:57:57 -0500

    First commit

---
+++ app/src/main/java/com/example/adminlivria/presentation/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.adminlivria.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.adminlivria.ui.theme.AdminLivriaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _root_ide_package_.com.example.adminlivria.presentation.ui.theme.AdminLivriaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _root_ide_package_.com.example.adminlivria.presentation.ui.theme.AdminLivriaTheme {
+        Greeting("Android")
+    }
+}
