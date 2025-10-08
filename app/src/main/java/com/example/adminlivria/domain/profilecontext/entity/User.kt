781e04cf4c77b47fc021f2d5647852ff2afcbda7
kcc12321 <cassiusmartel21042006@gmail.com>
2025-10-08 13:31:54 -0500

    Add User Entity

---
+++ app/src/main/java/com/example/adminlivria/domain/profilecontext/entity/User.kt
@@ -0,0 +1,9 @@
+package com.example.adminlivria.domain.profilecontext.entity
+
+// Clase abstracta o abierta para que otras entidades hereden de ella
+open class User(
+    open val username: String, // users.Username
+    open val fullName: String, // users.Display
+    open val email: String // users.Email
+
+)
